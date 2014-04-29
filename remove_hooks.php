fc18d44a94a3461eb33926ba93801305fe370f97
Shitiz Garg <mail@dragooon.net>
2014-04-29 19:45:37 +0530

    Add code to remove instilled hooks on uninstall

---
+++ remove_hooks.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Removes hooks which are instilled by mentions mod
+ *
+ * @author Shitiz Garg <mail@dragooon.net>
+ * @copyright 2014 Shitiz Garg
+ * @license Simplified BSD (2-Clause) License
+ */
+
+if (!defined('SMF'))
+    require_once('SSI.php');
+
+$hooks = array(
+    'integrate_pre_include' => '$sourcedir/Mentions.php',
+    'integrate_profile_areas' => 'mentions_profile_areas',
+    'integrate_load_permissions' => 'mentions_permissions',
+    'integrate_bbc_codes' => 'mentions_bbc',
+    'integrate_menu_buttons' => 'mentions_menu',
+);
+
+foreach ($hooks as $hook => $function)
+    remove_integration_function($hook, $function);
